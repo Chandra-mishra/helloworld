5b42cf0411906aea9a3d025d42ace1d33bc005a8
Chandra Mishra <Prasadchandra18@gmail.com>
2018-12-27 16:12:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 app.set('view engine','ejs');
-app.GET('/hello',function(req,res){
+app.get('/hello',function(req,res){
     res.render('cpm');
 });
 app.listen(port, function() {
